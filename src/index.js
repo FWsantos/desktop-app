e4552b40c9e3d2d7e78820908dddd68c1b8c6afb
FWsantos <fwsantos23@gmail.com>
2023-05-05 14:40:36 -0300

    electron adds

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // win.loadFile(path.join(__dirname, "../dist/index.html"));
+  win.loadURL("http://localhost:5173/");
+  // win.loadURL("http://tiktok.com");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
