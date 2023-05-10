d48e74dc4ad94e3b8f415c4e242f336698d4f0b2
FWsantos <fwsantos23@gmail.com>
2023-05-09 23:25:43 -0300

    List component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
-import ChatNotification from "./componentes/ChatNotification";
+import List from "./componentes/List";
 
 function App() {
+  const lista = [
+    "item 01",
+    "item 02",
+    "item 03",
+    "item 04",
+    "item 05",
+    // "item 06",
+    // "item 07",
+    // "item 08",
+    // "item 09",
+  ];
   return (
     <>
-      <main className="">
-        <ChatNotification></ChatNotification>
+      <main className="w-3/4 hscreen mx-auto mt-8 p-8">
+        <h1 className="mb-4">Texto titulo</h1>
+        <List itens={lista}></List>
       </main>
     </>
   );
